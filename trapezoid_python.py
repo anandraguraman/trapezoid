c84ea3dee88cb6a5e0528bd36fd6248de6ae97da
anandraguraman <113701095+anandraguraman@users.noreply.github.com>
2022-10-22 10:26:37 -0400

    Add files via upload

---
+++ trapezoid_python.py
@@ -0,0 +1,10 @@
+print("Let's calculate the area of trapezoid")
+
+baseA = int(input("Enter base length: "))
+baseB = int(input("Enter the other base length: "))
+
+height = int(input("Enter height: "))
+
+area = (((baseA + baseB) / 2) * height)
+
+print("area of trapezoid is: ", area)
